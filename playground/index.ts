49b8abe3e9f8e93a3ee9fe9e8731b40efaeb70ce
Nekmo <contacto@nekmo.com>
2018-06-14 02:34:02 +0200

    Material components files.

--- playground/index.ts
+++ playground/index.ts
@@ -6,7 +6,7 @@ import { NgModule } from '@angular/core';
 import { Component } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { SampleModule }  from '@angular/django';
+import { SampleModule }  from 'angular-django';
 
 @Component({
   selector: 'app',
--- playground/systemjs.config.js
+++ playground/systemjs.config.js
@@ -27,7 +27,7 @@
       // other libraries
       rxjs: 'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
-      '@angular/django': '../dist'
+      'angular-django': '../dist'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -45,7 +45,7 @@
       rxjs: {
         defaultExtension: 'js'
       },
-      '@angular/django': {
+      'angular-django': {
         main: 'django.umd.js',
         defaultExtension: 'js'
       }
